bc0cc2846498f5b42f62430a096f6ed0dbd77a2e
Sergio Di Fiore <difiores@outlook.com>
2019-04-04 15:59:27 -0300

    Add project files.

---
+++ UsandoCookies/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsandoCookies.Models;
+
+namespace UsandoCookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new Mensagem());
+        }
+
+        public IActionResult WriteCookie(Mensagem mensagem)
+        {
+            Response.Cookies.Append("CookieComAMensagem", mensagem.Entrada);
+
+            return RedirectToAction("Leitura");
+        }
+
+        public IActionResult Leitura()
+        {
+            var resultado = new Mensagem();
+            resultado.Saida = Request.Cookies["CookieComAMensagem"];
+
+            return View(resultado);
+        }
+    }
+}
---
+++ UsandoCookies/Models/Mensagem.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsandoCookies.Models
+{
+    public class Mensagem
+    {
+        [StringLength(4096)]
+        [Display(Name = "Digite o texto a ser gravado:")]
+        [Required(ErrorMessage = "Comprimento máximo de 256 caracteres")]
+        public string Entrada { get; set; }
+
+        [Display(Name = "Resultado lido:")]
+        public string Saida { get; set; }
+    }
+}
